5efb858e32489de639f72a10ac7e961cf10c6feb
evanwill <evanpeterw@gmail.com>
2018-04-26 09:01:54 -0700

    update git initializers varibles

--- config/initializers/git_sha.rb
+++ config/initializers/git_sha.rb
@@ -11,3 +11,10 @@ BRANCH =
   else
     "Unknown branch"
   end
+
+GIT_DATE =
+  if Rails.env.development? || Rails.env.test?
+    `git log -1 --date=short --pretty=format:%cd`.chomp
+  else
+    "Unknown date"
+  end
